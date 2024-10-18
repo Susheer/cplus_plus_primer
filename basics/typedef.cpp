3ce541973e7be826aa95c2249c493ceff28c9ad4
sudheer <sudheergupta0508@gmail.com>
2024-10-18 17:51:09 +0530

    Use of typedef

---
+++ basics/typedef.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+typedef std::string text_t;
+
+int main(){
+    std::string name;
+    text_t address; 
+
+    name = "Sudheer gupta";
+    address = "USA Tx";
+
+    cout<<"pring name "<<name;
+    cout<<"\n";
+
+    cout <<"Address-"<<address;
+     
+    return 0;
+}
+
+
+// Note - Line number 7,8 server the same perpose;
